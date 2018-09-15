dfadca5a0c1a1399f931fdeede2c20dca2ce13a5
Andrew B <awb305@gmail.com>
2018-09-15 14:51:14 -0400

    put pictures in array

--- src/componets/container/container.jsx
+++ src/componets/container/container.jsx
@@ -15,41 +15,70 @@ const styles = {
   }
 };
 
+
+const images = [
+  {
+    name: 'Jordan',
+    link: '/images/jordan.jpeg'
+  },
+  {
+    name: 'Antawn',
+    link: '/images/antawn.jpeg',
+  }, 
+  {
+    name: 'Billy',
+    link: '/images/billy.jpg'
+  }, 
+  {
+    name: 'Dean',
+    link: '/images/dean.jpg'
+  },
+  {
+    name: 'Ford',
+    link: '/images/ford.jpg'
+  }, 
+  {
+    name: 'James',
+    link: '/images/james.jpeg'
+  }, 
+  {
+    name: 'Justin',
+    link: '/images/justin.jpg'
+  }, 
+  {
+    name: 'Marcus',
+    link: '/images/marcus.jpg'
+  },
+  {
+    name: 'Vince',
+    link: '/images/vince.jpg'
+  } 
+];
+
+let column =  images.map(image => (
+  <Col xs={6} md={4} style={styles.col}>
+    <Image className = "pic-UNC" alt={image.name} src={image.link} thumbnail />
+  </Col> 
+ ));
+
+
 const Container= ()=> (
 
 <Grid fluid style={styles.grid}>
   <Row style={styles.row}>
-    <Col xs={6} md={4} style={styles.col}>
-      <Image className = "pic-UNC" src="/images/jordan.jpeg" thumbnail />
-    </Col>
-    <Col xs={6} md={4} style={styles.col}>
-      <Image className = "pic-UNC" src="/images/antawn.jpeg" thumbnail />
-    </Col>
-    <Col xs={6} md={4} style={styles.col}>
-      <Image className = "pic-UNC" src="/images/billy.jpg" thumbnail />
-    </Col>
+    {column[0]}
+    {column[1]}
+    {column[2]}
   </Row>
   <Row style={styles.row}>
-    <Col xs={6} md={4} style={styles.col}>
-      <Image className = "pic-UNC" src="/images/dean.jpg" thumbnail />
-    </Col>
-    <Col xs={6} md={4} style={styles.col}>
-      <Image className = "pic-UNC" src="/images/ford.jpg" thumbnail />
-    </Col>
-    <Col xs={6} md={4} style={styles.col}>
-      <Image className = "pic-UNC" src="/images/james.jpeg" thumbnail />
-    </Col>
+    {column[3]}
+    {column[4]}
+    {column[5]}
   </Row>
   <Row style={styles.row}>
-    <Col xs={6} md={4} style={styles.col}>
-      <Image className = "pic-UNC" src="/images/justin.jpg" thumbnail />
-    </Col>
-    <Col xs={6} md={4} style={styles.col}>
-      <Image className = "pic-UNC" src="/images/marcus.jpg" thumbnail />
-    </Col>
-    <Col xs={6} md={4} style={styles.col}>
-      <Image className = "pic-UNC" src="/images/vince.jpg" thumbnail />
-    </Col>
+    {column[6]}
+    {column[7]}
+    {column[8]}
   </Row>
 </Grid>
 
